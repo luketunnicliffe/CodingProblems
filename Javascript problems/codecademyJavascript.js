48a8c5ca3b5cf00631160811a758833550ca7f49
tekipakicoder <luke@eigowithluke.com>
2021-01-01 23:10:48 +0900

    Added another function

--- Javascript problems/codecademyJavascript.js
+++ Javascript problems/codecademyJavascript.js
@@ -1,4 +1,18 @@
-<h1>Hello</h1>
+
+function convertToBaby(animals) {
+    console.log(animals)
+    for (let animal = 1; animal < animals.length; animal++) {
+      return `baby ${animals[animal]}`
+    
+    }
+    
+    }
+
+
+const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
+
+console.log(convertToBaby(animals)) 
+
 
 function javaScriptFunction(){`
     console.log('Hello');
